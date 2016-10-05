68d2f6983155ebb708bb4eeddc0c2b33a5971c20
CH33ZED <zlin3@stuy.edu>
2016-10-05 00:23:05 -0400

    Done

---
+++ utils/register.py
@@ -0,0 +1,29 @@
+from flask import request
+import csv
+import hashlib
+
+def hesh(a):
+    return(hashlib.md5(a).hexdigest())
+
+def regi(a, b):
+    c = csv.reader(open("name.csv"))
+    for d in c:
+        if a == d[0]:
+            return "Name Taken!"
+    with open('name.csv', 'a') as e:
+        f = csv.writer(e)
+        f.writerow([a, hesh(b)])
+    return "Added"
+             
+
+def login(a, b):
+    c = csv.reader(open("name.csv"))
+    for d in c:
+        if a == d[0]:
+            if hesh(b)==d[1]:
+                return "Welcome"
+            return "Incorred Password"
+        return "You need to register"
+
+
+#did look at some sample codes as basis
